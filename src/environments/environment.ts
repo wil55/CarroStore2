635973d080a91a8c118b3fb45ac150e2c29ea41b
wil55 <wiliamcoutocosta@gmail.com>
2020-07-08 22:15:59 -0300

    5 commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  
   production: false, 
   firebaseConfig:  {
     apiKey: "AIzaSyBoZgHBAyEOXJQvaCvEbzUqF1zc1T3Alsc",
